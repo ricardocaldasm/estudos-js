47ee6dd2ace53811344e6dd2c9447d66c185611e
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2023-03-18 17:07:10 -0300

    ex 8 9 e 10

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do carro é ${vel}km/h`)
+if (vel > 60) { // Condição simples
+    console.log(`Multado`)
+}
+console.log(`Dirija com segurança.`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais != 'Brasil') {
+    console.log(`Estrangeiro`)
+} else {
+    console.log(`Brasileiro`)
+}
