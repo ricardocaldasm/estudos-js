711e836afa7a97630c84008ff40528686b158bf6
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2023-03-19 17:55:12 -0300

    Fim ex014

--- ex001/ex014/script.js
+++ ex001/ex014/script.js
@@ -4,4 +4,14 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >=0 && hora <12) {
+        img.src = 'morning-p.png' //229 209 184
+        document.body.style.background = '#e5d1b8' //não funciona rgb nem background-color
+    } else if (hora >=12 && hora<18) {
+        img.src = 'afternoon-p.png' //197 234 243
+        document.body.style.background = '#c5eaf3'
+    } else {
+        img.src = 'night-p.png' //64 79 86
+        document.body.style.background = '#404f56'
+    }
 }
