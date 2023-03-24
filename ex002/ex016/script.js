640dca471ae0eb134400995e52a758c33cd20ca3
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2023-03-24 19:42:03 -0300

    ex016

--- ex002/ex016/script.js
+++ ex002/ex016/script.js
@@ -1,2 +1,13 @@
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+    }
+}
 
