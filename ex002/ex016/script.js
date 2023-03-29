4337f0f9b67df58bf336339b6b4547b9ff884e89
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2023-03-29 15:01:24 -0300

    ex016 finalizado

--- ex002/ex016/script.js
+++ ex002/ex016/script.js
@@ -5,9 +5,29 @@ function contar() {
     var res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value) //let pode ser substituido por var
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            // contagem crescente
+            for(let c = i; c <= f; c += p) {//  ou c = c + p
+                res.innerHTML += `${c} \u{1F449} ` //+= neste caso é concatenação
+            }
+        } else {
+            // contagem descrescente
+            for (c = i; c >= f; c -=p) {
+                res.innerText += `${c} \u{1F449}`
+            }
+        }
+        res.innerText += `\u{1F3C1}`
     }
 }
 
